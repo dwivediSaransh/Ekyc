b64bdca99eac8a38b285bc9ce265900147ae8c73
shanawaz khan <shanawazumarkhan@gmail.com>
2021-07-26 15:39:40 +0530

    26-07-21

---
+++ precache-manifest.5617cd41212baeefc1e95f44ee73abd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bffb3305a2a72f58120ba12c374b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d5d9e6ef3a3836a9ad8",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "44f54cfa077d5595e5c5",
+    "url": "/static/css/main.15fd2f71.chunk.css"
+  },
+  {
+    "revision": "3d5d9e6ef3a3836a9ad8",
+    "url": "/static/js/2.be92b339.chunk.js"
+  },
+  {
+    "revision": "44f54cfa077d5595e5c5",
+    "url": "/static/js/main.566f7e35.chunk.js"
+  },
+  {
+    "revision": "77ab58062fd0b4c10187",
+    "url": "/static/js/runtime-main.498fbaad.js"
+  }
+]);
